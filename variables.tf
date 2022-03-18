2fea336ccd8da1681abb5ad1acd1971f80c3ad6f
Gordon Adam-Santiago <calxus@protonmail.ch>
2022-03-18 09:19:50 +0000

    Adding the initial deployment

---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "service" {
+  description = "The name of the service being deployed"
+  type        = string
+}
+
+variable "location" {
+  description = "The location the service is being deployed to"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "The location the service is being deployed to"
+  type        = string
+}
+
+variable "subnet" {
+  description = "The subnet the service is being deployed to"
+  type        = string
+}
+
+variable "registry" {
+  description = "The registry the image will be pulled from"
+  type        = object({
+    username = number
+    password = number
+    server   = string
+  })
+}
+
+variable "container" {
+  description = "The details of the container that is being deployed"
+  type        = object({
+    cpu         = number
+    memory      = number
+    image       = string
+    environment = list(map(string))
+    port        = number
+    platfform   = string
+  })
+}
